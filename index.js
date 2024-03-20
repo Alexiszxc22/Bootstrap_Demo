64710455c86f01d80af0c3f5f4397eb93b5fb21e
Alexiszxc22 <alexiszxc.wup@gmail.com>
2024-03-20 11:50:48 +0800

    edit the console log

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ function generateNumber(maxNumber) {
     if (randomNumber <= 15) {
         BNumber = randomNumber;
         isB = true;
-        console.log('The Number ${randomNumber} is in "B"');
+        console.log(`The Number ${randomNumber} is in "B"`);
 
     } else if (randomNumber >= 16 && randomNumber <= 30) {
         INumber = randomNumber;
         isI = true;
-        console.log('The Number ${randomNumber} is in "I"');
+        console.log(`The Number ${randomNumber} is in "I`);
 
     } else if (randomNumber >= 31 && randomNumber <= 45) {
         NNumber = randomNumber;
         isN = true;
-        console.log('The Number ${randomNumber} is in "N"');
+        console.log(`The Number ${randomNumber} is in "N"`);
 
     } else if (randomNumber >= 46 && randomNumber <= 60) {
         GNumber = randomNumber;
         isG = true;
-        console.log('The Number is ${randomNumber} is in "G"');
+        console.log(`The Number is ${randomNumber} is in "G"`);
 
     } else if (randomNumber >= 61 && randomNumber <= 75) {
         ONumber = randomNumber;
         isO = true;
-        console.log('The Number ${randomNumber} is in"O"');
+        console.log(`The Number ${randomNumber} is in"O"`);
 
     } else {
         console.log('The number is invalid.');
