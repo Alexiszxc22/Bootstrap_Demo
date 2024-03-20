682f10bda7881bcc798971bf945a4b937a26f18d
Alexiszxc22 <alexiszxc.wup@gmail.com>
2024-03-20 11:34:01 +0800

    The new Bingo table function

--- index.js
+++ index.js
@@ -1,54 +1,76 @@
 console.log("Hello World");
-let isB = false, 
-isI = false,
-isN = false, 
-isG = false,
-isO = false;
+let isB = false, isI = false, isN = false, isG = false, isO = false;
+
+let BNumber, INumber, NNumber, GNumber, ONumber;
+
+
 
 //new number
 
-function generateNumber (maxNumber) {
-    
-    
+function generateNumber(maxNumber) {
+
+
     // Addition = "+"
     // Subtraction = "-"
     // Multiplication = "*"
     // Division = "/"
     // Remainder = "%"
 
-   let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+    let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
 
-   // if ... else if ... else statement
-   // Conditional operator
-   // GE >=, LTE <=, EQ ==, NEQ ! =, AND &&, OR ||
+    // if ... else if ... else statement
+    // Conditional operator
+    // GE >=, LTE <=, EQ ==, NEQ ! =, AND &&, OR ||
     if (randomNumber <= 15) {
+        BNumber = randomNumber;
         isB = true;
-        console.log('The Number is in "B"');
-        
-    } else if (randomNumber>= 16 && randomNumber <= 30) {
+        console.log('The Number ${randomNumber} is in "B"');
+
+    } else if (randomNumber >= 16 && randomNumber <= 30) {
+        INumber = randomNumber;
         isI = true;
-        console.log('The Number is in "I"');
-        
-    } else if (randomNumber>= 31 && randomNumber <= 45) {
-       isN = true;
-       console.log('The Number is in "N"');
-        
+        console.log('The Number ${randomNumber} is in "I"');
+
+    } else if (randomNumber >= 31 && randomNumber <= 45) {
+        NNumber = randomNumber;
+        isN = true;
+        console.log('The Number is ${randomNumber} in "N"');
+
     } else if (randomNumber >= 46 && randomNumber <= 60) {
+        GNumber = randomNumber;
         isG = true;
-        console.log('The Number is in "G"');
-        
+        console.log('The Number is ${randomNumber} in "G"');
+
     } else if (randomNumber >= 61 && randomNumber <= 75) {
+        ONumber = randomNumber;
         isO = true;
-        console.log('The Number is in"O"');
-        
+        console.log('The Number ${randomNumber} is in"O"');
+
     } else {
         console.log('The number is invalid.');
     }
 
-    if (isB == true && isI==true && isN == true && isG == true && isO == true) {
-     console.log('BINGOOOOOOO!!!!');   
-     
+    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
+        console.log('BEEENGOOOOOOOOOO!!!!');
+        console.table({
+            B: BNumber,
+            I: INumber,
+            N: NNumber,
+            G: GNumber,
+            O: ONumber,
+        })
     }
+
 }
 
+
 let counter = 10;
+
+while (
+    isB == false ||
+    isI == false ||
+    isN == false ||
+    isG == false ||
+    isO == false) {
+    generateNumber(75)
+}
